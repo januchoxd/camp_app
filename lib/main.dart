130ea2d4bf6660e092cd7c95cc5dc7cb830aca72
janusz jas <janusz@pietkun.pl>
2023-02-09 19:04:39 +0100

    czwartek

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
 
 /// Przenosi od razu do LoginScreen
+/// esa
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
